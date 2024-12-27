232777d7d491318407019c0c035307a6882b0dda
Szymon Molitorys <szymon.molitorys@gmail.com>
2024-12-27 09:24:15 +0100

    feat: class Timer with constructor which starts timer

---
+++ Pieciolinia/Source/Common/Timer.cpp
@@ -0,0 +1,23 @@
+/*
+  ==============================================================================
+
+    Timer.cpp
+    Created: 27 Dec 2024 9:22:15am
+    Author:  Lenovo
+
+  ==============================================================================
+*/
+
+#include "Timer.h"
+#include <chrono>
+#include <thread>
+
+
+class Timer {
+public:
+    Timer() {
+        start_time = std::chrono::high_resolution_clock::now();
+    }
+private:
+    std::chrono::time_point<std::chrono::high_resolution_clock> start_time;
+};
