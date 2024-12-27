eb2a854676323f3f31bbdb965c967e5c9af18538
Szymon Molitorys <szymon.molitorys@gmail.com>
2024-12-27 10:15:51 +0100

    feat: all notes we will use in NoteHeight

--- Pieciolinia/Source/Common/Note.cpp
+++ Pieciolinia/Source/Common/Note.cpp
@@ -17,3 +17,26 @@ enum class NoteLength {
     Eighth,   
     Sixteenth 
 };
+
+enum class NoteHeigth {
+    c1,
+    cSh1,
+    d1,
+    dSh1,
+    e1,
+    f1,
+    fSh1,
+    g1,
+    gSh1,
+    a1,
+    aSh1,
+    h1,
+    c2,
+    cSh2,
+    d2,
+    dSh2,
+    e2,
+    f2,
+    fSh2,
+    g2,
+};
