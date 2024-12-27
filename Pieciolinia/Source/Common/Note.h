3901dca4df3f0e647214ca465e1a35e0a300a422
Szymon Molitorys <szymon.molitorys@gmail.com>
2024-12-27 10:09:20 +0100

    feat: class Note; initial ideas for .h file and methods there

---
+++ Pieciolinia/Source/Common/Note.h
@@ -0,0 +1,29 @@
+/*
+  ==============================================================================
+
+    Note.h
+    Created: 27 Dec 2024 9:59:38am
+    Author:  Lenovo
+
+  ==============================================================================
+*/
+
+#pragma once
+#ifndef NOTE_H
+#define NOTE_H
+#include <string>
+
+class Note {
+public:
+
+    Note();
+
+    enum class NoteLength {};
+
+    enum class NoteName {};
+
+    struct noteInfo {};
+
+};
+
+#endif 
