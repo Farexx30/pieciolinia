b09087598e13c77aa608ab190980b7ba9c4d6cb7
Szymon Molitorys <szymon.molitorys@gmail.com>
2024-12-27 09:31:17 +0100

    feat: method returning time in miliseconds

--- Pieciolinia/Source/Common/Timer.cpp
+++ Pieciolinia/Source/Common/Timer.cpp
@@ -24,6 +24,12 @@ public:
     void reset() {
         start_time = std::chrono::high_resolution_clock::now();
     }
+
+    double elapsedMilliseconds() const {
+        return std::chrono::duration<double, std::milli>(
+            std::chrono::high_resolution_clock::now() - start_time
+        ).count();
+    }
 private:
     std::chrono::time_point<std::chrono::high_resolution_clock> start_time;
 };
