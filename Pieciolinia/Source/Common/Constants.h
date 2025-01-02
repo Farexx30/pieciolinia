dacae0486c9fa2a6968013a3cbb9f481eef8e48e
Szymon Molitorys <szymon.molitorys@gmail.com>
2025-01-02 11:37:48 +0100

    fix: in the font notes have different keys

--- Pieciolinia/Source/Common/Constants.h
+++ Pieciolinia/Source/Common/Constants.h
@@ -24,16 +24,8 @@ namespace LabelConstants
 namespace KeyToNoteMapping
 {
     inline std::unordered_map<char, Note::NoteName> keyToNote = {
-        {'z', Note::NoteName::c1}, {'s', Note::NoteName::cSharp1},
-        {'x', Note::NoteName::d1}, {'d', Note::NoteName::dSharp1},
-        {'c', Note::NoteName::e1}, {'v', Note::NoteName::f1},
-        {'g', Note::NoteName::fSharp1}, {'b', Note::NoteName::g1},
-        {'h', Note::NoteName::gSharp1}, {'n', Note::NoteName::a1},
-        {'j', Note::NoteName::aSharp1}, {'m', Note::NoteName::h1},
-        {'q', Note::NoteName::c2}, {'2', Note::NoteName::cSharp2},
-        {'w', Note::NoteName::d2}, {'3', Note::NoteName::dSharp2},
-        {'e', Note::NoteName::e2}, {'r', Note::NoteName::f2},
-        {'5', Note::NoteName::fSharp2}, {'t', Note::NoteName::g2}
+        {'B', Note::NoteName::c1},
+        
     };
 }
 namespace CompositionConstants
