f8fbf773a5d6e195b4a867b5e5317c97d11e4af8
Szymon Molitorys <szymon.molitorys@gmail.com>
2025-01-09 18:55:04 +0100

    feat: function HandleKeyPress

--- Pieciolinia/Source/Common/KeyboardInputHandler.cpp
+++ Pieciolinia/Source/Common/KeyboardInputHandler.cpp
@@ -9,3 +9,22 @@
 */
 
 #include "KeyboardInputHandler.h"
+#include "Constants.h"
+#include "Note.h"
+#include "Note.cpp"
+#include <iostream>
+
+void handleKeyPress(char key) {
+    auto it = KeyToNoteMapping::keyToNote.find(key);
+    if (it != KeyToNoteMapping::keyToNote.end()) {
+        Note note;
+        note.info.name = it->second;
+
+        CompositionConstants::notes.push_back(note);
+
+        std::cout << "Added note: " << static_cast<int>(note.info.name) << std::endl;
+    }
+    else {
+        std::cerr << "Invalid key pressed: " << key << std::endl;
+    }
+}
